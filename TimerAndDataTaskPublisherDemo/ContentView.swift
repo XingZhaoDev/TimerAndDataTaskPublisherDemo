0fc905dcf44047c836d7ac3be5a3c51a2fff7a30
XING <zhaoxing8325@gmail.com>
2021-04-30 13:01:04 -0700

    Initial Commit

---
+++ TimerAndDataTaskPublisherDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimerAndDataTaskPublisherDemo
+//
+//  Created by Xing Zhao on 2021/4/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimerAndDataTaskPublisherDemo/TimerAndDataTaskPublisherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAndDataTaskPublisherDemoApp.swift
+//  TimerAndDataTaskPublisherDemo
+//
+//  Created by Xing Zhao on 2021/4/30.
+//
+
+import SwiftUI
+
+@main
+struct TimerAndDataTaskPublisherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
